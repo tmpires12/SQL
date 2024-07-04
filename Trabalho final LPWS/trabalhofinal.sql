d9970989d3bbb5465ba2d2d8836287f42ce638a5
tmpires12 <tmpires87@gmail.com>
2024-07-04 15:33:58 +0100

    Update trabalhofinal.sql

--- Trabalho final LPWS/trabalhofinal.sql
+++ Trabalho final LPWS/trabalhofinal.sql
@@ -20,16 +20,41 @@ Create DataBAse Loja;
    
     Create TABLE Fornecedores (
         id_fornecedor int,
+        Nome_fornecedor text,
         local_do_fornecedor text,
         valor_a_pagar text,
         distribuidora text )        
 
-        INSERT INTO clientes (id, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (780, 647990, 'Jose', 'Setubal', '789234789')
-        INSERT INTO clientes (id, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (952, 632723, 'Antonio', 'Albufeira', '435879087')
-        INSERT INTO clientes (id, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (867, 362894, 'Carlos', 'Benfica', '32189765')
-        INSERT INTO clientes (id, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (566, 876452, 'Hugo', 'Vila_de_Rei', '915652530')
-        INSERT INTO clientes (id, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (100, 167435, 'Sofia', 'Porto', '457764322')
-        INSERT INTO clientes (id, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (878, 471461, 'Patricia', 'Braga', '765432098')
-        INSERT INTO clientes (id, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (75, 387489, 'Carla', 'Madrid', '325687909')
-        INSERT INTO clientes (id, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (777, 483264, 'Camila', 'Braganca', '123456789')
+------------Clientes---------------
+        INSERT INTO clientes (id_cliente, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (780, 647990, 'Jose', 'Setubal', '789234789')
+        INSERT INTO clientes (id_cliente, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (952, 632723, 'Antonio', 'Albufeira', '435879087')
+        INSERT INTO clientes (id_cliente, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (867, 362894, 'Carlos', 'Benfica', '32189765')
+        INSERT INTO clientes (id_cliente, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (566, 876452, 'Hugo', 'Vila_de_Rei', '915652530')
+        INSERT INTO clientes (id_cliente, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (100, 167435, 'Sofia', 'Porto', '457764322')
+        INSERT INTO clientes (id_cliente, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (878, 471461, 'Patricia', 'Braga', '765432098')
+        INSERT INTO clientes (id_cliente, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (75, 387489, 'Carla', 'Madrid', '325687909')
+        INSERT INTO clientes (id_cliente, numero_da_encomenda, Nome_do_CLiente, Morada, Telemovel) VALUES (777, 483264, 'Camila', 'Braganca', '123456789')
 
+-------------Encomendas------------
+         INSERT into Encomendas (id_encomenda, id_cliente, numero_da_encomenda) VALUES (647990, 780, 7948598)
+        iNSERT into Encomendas (id_encomenda, id_cliente, numero_da_encomenda) VALUES (632723, 952, 893243472)
+        iNSERT into Encomendas (id_encomenda, id_cliente, numero_da_encomenda) VALUES (362894, 867, 64207864)
+        iNSERT into Encomendas (id_encomenda, id_cliente, numero_da_encomenda) VALUES (876452, 566, 87392867)
+        iNSERT into Encomendas (id_encomenda, id_cliente, numero_da_encomenda) VALUES (167435, 100, 128749217)
+        iNSERT into Encomendas (id_encomenda, id_cliente, numero_da_encomenda) VALUES (471461, 878, 6107463)
+        iNSERT into Encomendas (id_encomenda, id_cliente, numero_da_encomenda) VALUES (387489, 75, 138731389)
+        iNSERT into Encomendas (id_encomenda, id_cliente, numero_da_encomenda) VALUES (483264, 777, 72860423)
+
+-----------Funcionarios----------
+    INSERT into Funcionarios (ID_FUNCIONARIO, NOME_Funcionrio, LOCAL_DE_TRABALHO, funcao_do_funcionario) VALUES (765, 'Igor', 'Lisboa', 'Distribuidor')
+    INSERT into Funcionarios (ID_FUNCIONARIO, NOME_Funcionrio, LOCAL_DE_TRABALHO, funcao_do_funcionario) VALUES (40, 'Mario', 'Porto', 'Chefe Europeu')
+    INSERT into Funcionarios (ID_FUNCIONARIO, NOME_Funcionrio, LOCAL_DE_TRABALHO, funcao_do_funcionario) VALUES (999, 'Guilherme', 'Madrid', 'Distribuidor')
+    INSERT into Funcionarios (ID_FUNCIONARIO, NOME_Funcionrio, LOCAL_DE_TRABALHO, funcao_do_funcionario) VALUES (555, 'Felipe', 'Barcelona', 'Distribuidor')
+    INSERT into Funcionarios (ID_FUNCIONARIO, NOME_Funcionrio, LOCAL_DE_TRABALHO, funcao_do_funcionario) VALUES (1, 'Miguel', 'Londres', 'Dono')
+
+----------Fornecedores--------
+    INSERT into Fornecedores (id_fornecedor, Nome_fornecedor, local_do_fornecedor, valor_a_pagar, distribuidora) VALUES (877, 'Primor', 'Espanha', '100000€', 'DHL' )
+    INSERT into Fornecedores (id_fornecedor, Nome_fornecedor, local_do_fornecedor, valor_a_pagar, distribuidora) VALUES (999, 'Perfumes&Companhia', '5032€', 'DPD')
+    INSERT into Fornecedores (id_fornecedor, Nome_fornecedor, local_do_fornecedor, valor_a_pagar, distribuidora) VALUES (10, 'Aliexpress', 'China', '500€', 'CTT')
+    INSERT into Fornecedores (id_fornecedor, Nome_fornecedor, local_do_fornecedor, valor_a_pagar, distribuidora) VALUES (65, 'Amazon', 'França', '1000€', 'DHL')
+    INSERT into Fornecedores (id_fornecedor, Nome_fornecedor, local_do_fornecedor, valor_a_pagar, distribuidora) VALUES (444, 'AllBeuty', 'Inglaterra', '50000€', 'NACEX')
